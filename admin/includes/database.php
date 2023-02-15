07225be9be6193819b4e2f7758c6a9c1446b7360
Beatrice Tan <joanb.tan@gmail.com>
2023-02-15 16:25:05 -0500

    initial commit

---
+++ admin/includes/database.php
@@ -0,0 +1,10 @@
+<?php
+
+$connect = mysqli_connect( 
+    "sql201.epizy.com", //host
+    "epiz_33365969", //username
+    "8Yd7Cia0B1s",  //password
+    "epiz_33365969_cms" //DATABASE
+);
+
+mysqli_set_charset( $connect, 'UTF8' );
